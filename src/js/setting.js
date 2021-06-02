f903f9dcc0ec329515fd6bfa3ca15f5dd01ccd5c
uzunhair <uzunhair@yandex.ru>
2021-06-02 06:42:47 +0300

    Add new code

--- src/js/setting.js
+++ src/js/setting.js
@@ -10,3 +10,17 @@ import $ from 'jquery';
 //     $('[data-toggle="popover"]').popover();
 //   });
 // });
+
+const navbarToggler = document.querySelector('.navbar-toggler');
+const body = document.querySelector('body');
+const headerClose = document.querySelectorAll('.js_header__close');
+
+navbarToggler.addEventListener('click', () => {
+  body.classList.toggle('header__show');
+});
+
+for (const buttonItem of headerClose) {
+  buttonItem.addEventListener('click', () => {
+    navbarToggler.click();
+  });
+}
