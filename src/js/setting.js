5b77a480bb55f0eda7eadb305eefa8a4cc58d876
uzunhair <uzunhair@yandex.ru>
2021-06-02 06:51:06 +0300

    Disabled toggle navbar link

--- src/js/setting.js
+++ src/js/setting.js
@@ -1,26 +1,39 @@
-import $ from 'jquery';
-
-// $(document).ready(() => {
-//   $(() => {
-//     // Запускаем инициализацию tooltip
-//     $('[data-toggle="tooltip"]').tooltip();
-//   });
-//
-//   $(() => {
-//     $('[data-toggle="popover"]').popover();
-//   });
-// });
-
 const navbarToggler = document.querySelector('.navbar-toggler');
 const body = document.querySelector('body');
-const headerClose = document.querySelectorAll('.js_header__close');
+// const headerClose = document.querySelectorAll('.js_header__close');
 
 navbarToggler.addEventListener('click', () => {
   body.classList.toggle('header__show');
 });
 
-for (const buttonItem of headerClose) {
-  buttonItem.addEventListener('click', () => {
-    navbarToggler.click();
-  });
-}
+// (function () {
+//   const throttle = function (type, name, obj) {
+//     obj = obj || window;
+//     let running = false;
+//     const func = function () {
+//       if (running) { return; }
+//       running = true;
+//       requestAnimationFrame(() => {
+//         obj.dispatchEvent(new CustomEvent(name));
+//         running = false;
+//       });
+//     };
+//     obj.addEventListener(type, func);
+//   };
+//
+//   /* init - you can init any event */
+//   throttle('resize', 'optimizedResize');
+// }());
+//
+// // handle event
+// window.addEventListener('optimizedResize', () => {
+//   const pageWidth = document.documentElement.scrollWidth;
+//   console.log('Resource conscious resize callback!', pageWidth);
+//   if (pageWidth < 767) {
+//     for (const buttonItem of headerClose) {
+//       buttonItem.addEventListener('click', () => {
+//         navbarToggler.click();
+//       });
+//     }
+//   }
+// });
